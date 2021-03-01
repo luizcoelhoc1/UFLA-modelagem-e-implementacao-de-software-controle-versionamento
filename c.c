3b56456311305ce0b4e8efe2b5429800e65474d3
Luiz Carlos Coelho Conde <luizcoelhoc1@gmail.com>
2021-03-01 08:39:44 -0300

    mmc em c

---
+++ c.c
@@ -0,0 +1,25 @@
+//Código por Henrique Felipe (GitHub: HenriqueIni)
+#include <stdio.h>
+#include <stdlib.h>
+ 
+//Algoritmo de Euclides iterativo
+int mdc(int a, int b){
+    while(b != 0){
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+//Algoritmo do MMC
+int mmc(int a, int b){
+    return a * (b / mdc(a, b));
+}
+//Testes
+int main() {
+    printf("MMC\n");
+    printf("mmc(30, 60) = %d\n", mmc(30, 60));
+    printf("mmc(60, 100) = %d\n", mmc(60, 100));
+    printf("mmc(36, 14) = %d\n", mmc(36, 14));
+    return 0;
+}
